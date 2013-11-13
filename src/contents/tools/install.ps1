f8bca2862ad70d1fa0ad3f61de5a1759b9b16702
Robert Giesecke <robert.giesecke@gmail.com>
2013-11-13 18:28:40 +0100

    added install.ps1 and uninstall.ps1 to  /tools

---
+++ src/contents/tools/install.ps1
@@ -0,0 +1,28 @@
+param($installPath, $toolsPath, $package, $project)
+
+$targetFileName = 'CreateSnkFile.targets'
+$targetFullFileName = [IO.Path]::Combine($toolsPath, $targetFileName)
+$targetUri = New-Object Uri -ArgumentList $targetFullFileName, [UriKind]::Absolute
+
+$msBuildV4Name = 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a';
+$msBuildV4 = [System.Reflection.Assembly]::LoadWithPartialName($msBuildV4Name)
+
+if(!$msBuildV4) {
+    throw New-Object System.IO.FileNotFoundException("Could not load $msBuildV4Name.");
+}
+
+$msBuildV4.GetType('Microsoft.Build.Evaluation.ProjectCollection')::GlobalProjectCollection.GetLoadedProjects($project.FullName) | % {
+	$currentProject = $_
+
+	# remove imports from this project 
+	$currentProject.Xml.Imports | ? {
+		return ($targetFileName -ieq [IO.Path]::GetFileName($_.Project))
+	}  | % {  
+		$currentProject.Xml.RemoveChild($_);
+	}
+
+	$projectUri = New-Object Uri -ArgumentList $currentProject.FullPath, [UriKind]::Absolute
+	$relativeUrl = $projectUri.MakeRelative($targetUri)
+	$import = $currentProject.Xml.AddImport($relativeUrl)
+	$import.Condition = "Exists('$relativeUrl')";
+}
