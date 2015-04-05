4819b6e4d204bcd01a4f382401c078a9517b1e9a
Robert Giesecke <robert.giesecke@gmail.com>
2015-04-05 17:37:41 +0200

    created a classlibrary for the build task so that there's no dependency on CodeTaskFactory

---
+++ src/TaskLib/CreateSnkTask.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using Microsoft.Build.Utilities;
+
+namespace RGiesecke.CreateSnkFile.MSBuild
+{
+    public class CreateSnkTask : AppDomainIsolatedTask
+    {
+        public string SnkFilePath { get; set; }
+        public string SdkPath { get; set; }
+
+        public override bool Execute()
+        {
+            var fullSnkFileName = Path.GetFullPath(SnkFilePath);
+
+            if (!File.Exists(fullSnkFileName))
+            {
+                var args = string.Format(" -k \"{0}\"", fullSnkFileName);
+                var startInfo = new System.Diagnostics.ProcessStartInfo("sn", args)
+                {
+                    WorkingDirectory = SdkPath
+                };
+
+                using (var p = System.Diagnostics.Process.Start(startInfo))
+                {
+                    p.WaitForExit();
+                }
+            }
+
+            return true;
+        }
+    }
+}
